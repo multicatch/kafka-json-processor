d162d4385df990b89c41bb893f84d77f78e0fb77
multicatch <cezaryre@gmail.com>
2022-08-16 11:02:00 +0200

    produce messages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::Duration;
 use log::{error, info, LevelFilter};
 use rdkafka::{ClientConfig, Message};
 use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::error::KafkaError;
+use rdkafka::producer::{Producer, BaseProducer, BaseRecord};
 use tokio::runtime::Builder;
 
 fn main() {
@@ -23,8 +26,9 @@ fn main() {
         let config_path = "./processor.properties";
         let config_file = File::open(config_path).unwrap();
 
-        let consumer_conf = create_client_config(config_file).unwrap();
+        let (consumer_conf, producer_conf) = create_config(config_file).unwrap();
         let consumer: StreamConsumer = consumer_conf.create().unwrap();
+        let producer: BaseProducer = producer_conf.create().unwrap();
 
         consumer.subscribe(&["aaaa"]).unwrap();
 
@@ -32,9 +36,11 @@ fn main() {
             match consumer.recv().await {
                 Ok(message) => {
                     let payload = String::from_utf8_lossy(message.payload().unwrap());
-                    info!("Received message: [{}:{}:{}] [{}] {}",
-                        message.topic(), message.partition(), message.offset(), message.timestamp().to_millis().unwrap_or(0), payload
+                    let key = format!("{}:{}", message.topic(), message.partition());
+                    info!("Received message: [{}:{}] [{}] {}",
+                        key, message.offset(), message.timestamp().to_millis().unwrap_or(0), payload
                     );
+                    send(&producer, "bbbb", key, payload.into()).unwrap();
                 }
                 Err(e) => {
                     error!("Cannot consume message! Reason: {}", e);
@@ -44,21 +50,56 @@ fn main() {
     });
 }
 
-fn create_client_config(file: File) -> Result<ClientConfig, Box<dyn Error>> {
+fn create_config(file: File) -> Result<(ClientConfig, ClientConfig), Box<dyn Error>> {
     let mut consumer_config = ClientConfig::new();
+    let mut producer_config = ClientConfig::new();
 
     for line in BufReader::new(&file).lines() {
-        let cur_line: String = line?.trim().to_string();
-        if cur_line.starts_with('#') || cur_line.is_empty() {
+        let current_line: String = line?
+            .trim_start()
+            .to_string();
+
+        if current_line.starts_with('#') || current_line.trim().is_empty() {
             continue;
         }
 
-        let key_value: Vec<&str> = cur_line.split('=').collect();
-        let key = key_value.first().ok_or("malformed key")?;
-        let value = key_value.get(1).ok_or("malformed value")?;
+        let key_value: Vec<&str> = current_line.split('=').collect();
+        let key = key_value.first()
+            .ok_or(format!("Illegal config entry (malformed key): {current_line}"))?;
+        let value = key_value.get(1)
+            .ok_or(format!("Illegal config entry (malformed value): {current_line}"))?;
 
-        consumer_config.set(key.to_string(), value.to_string());
+        if key.starts_with("consumer.") {
+            consumer_config.set(
+                key.strip_prefix("consumer.").unwrap().to_string(),
+                value.to_string(),
+            );
+        } else if key.starts_with("producer.") {
+            producer_config.set(
+                key.strip_prefix("producer.").unwrap().to_string(),
+                value.to_string(),
+            );
+        } else {
+            consumer_config.set(key.to_string(), value.to_string());
+            producer_config.set(key.to_string(), value.to_string());
+        }
     }
 
-    Ok(consumer_config)
+    Ok((consumer_config, producer_config))
+}
+
+
+fn send(producer: &BaseProducer, topic: &str, key: String, payload: String) -> Result<(), KafkaError> {
+    let result = producer.send(
+        BaseRecord::to(topic)
+            .key(&key)
+            .payload(&payload),
+    );
+    if let Err((e, _)) = &result {
+        error!("Could not send message [{}]! Reason: {}, queue: {}", key, e, producer.in_flight_count());
+    } else {
+        producer.poll(Duration::from_millis(0));
+    }
+
+    result.map_err(|(e, _)| e)
 }
