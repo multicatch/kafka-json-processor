d613dba84ab9d1dc053466ac2d07a36a443fe7a2
multicatch <cezaryre@gmail.com>
2022-12-02 13:10:06 +0100

    add example for plugin and more documentation for --help

--- kjp-generator/src/main.rs
+++ kjp-generator/src/main.rs
@@ -21,19 +21,34 @@ fn main() {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to template file (YAML)
+    /// Path to template file (YAML).
+    ///
+    /// A template file is a configuration file that will be used to generate the final project.
+    /// 'The final project' - Rust project with generated code to process messages from selected Kafka topics.
     #[arg(short, long)]
     template: String,
 
-    /// Output directory of generated project
+    /// Output directory of generated project.
+    ///
+    /// This will be the directory where the project with message processors will be generated.
+    /// This project will contain generated code to process messages basing on the supplied template.
+    /// The code will need to be compiled afterwards.
     #[arg(short, long)]
     output: String,
 
-    /// Custom path to kafka_json_processor_core
+    /// Custom path to kafka_json_processor_core.
+    ///
+    /// kafka_json_processor_code is a dependency that contains code that will prevent boilerplate in the generated project.
+    /// By default, it will use hardcoded version from `crates.io`.
+    /// If it doesn't work (or you want to use custom core), supply a path to the source code of kafka_json_processor_code.
     #[arg(short, long)]
     core_path: Option<String>,
 
-    /// Custom path to processor generators
+    /// Custom path to processor generators.
+    ///
+    /// Put all processor generators in this directory.
+    /// This directory will be scanned for available files and those files will
+    /// be used as executable plugins to generate any code requested by `generator` option in your `template.yaml`.
     #[arg(short, long, default_value_t = String::from("./generators"))]
     generators_path: String,
 }
